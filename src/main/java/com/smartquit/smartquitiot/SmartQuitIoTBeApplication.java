c66cbb4a06adcb8b70d2c54c6562d88c1b3937c2
hailinh03 <hailinh2910203@gmail.com>
2025-09-20 13:29:39 +0700

    first commit

---
+++ src/main/java/com/smartquit/smartquitiot/SmartQuitIoTBeApplication.java
@@ -0,0 +1,13 @@
+package com.smartquit.smartquitiot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartQuitIoTBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartQuitIoTBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartquit/smartquitiot/SmartQuitIoTBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartquit.smartquitiot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartQuitIoTBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
