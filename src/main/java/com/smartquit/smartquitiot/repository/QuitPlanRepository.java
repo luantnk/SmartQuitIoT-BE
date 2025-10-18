6f98b105c3d4be287c8d90159ca712469a175629
hailinh03 <hailinh2910203@gmail.com>
2025-10-18 08:08:16 +0700

    fix: edit get using quit plan by member

--- src/main/java/com/smartquit/smartquitiot/repository/QuitPlanRepository.java
+++ src/main/java/com/smartquit/smartquitiot/repository/QuitPlanRepository.java
@@ -7,9 +7,10 @@ import com.smartquit.smartquitiot.toolcalling.QuitPlanTools;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public interface QuitPlanRepository extends JpaRepository<QuitPlan,Integer> {
     QuitPlan findByStatus(QuitPlanStatus status);
     QuitPlan findTopByOrderByCreatedAtDesc();
-
+    QuitPlan findByMember_IdAndStatus(int memberId, QuitPlanStatus status);
 }
