6cea35f166bc7a2d5bec72464664868fffab3e29
gitdev-mdat <thiminhdatdaknong@gmail.com>
2025-12-23 10:21:22 +0700

    add package test

---
+++ src/test/java/com/smartquit/smartquitiot/service/impl/AppointmentServiceImplTest.java
@@ -0,0 +1,10 @@
+package com.smartquit.smartquitiot.service.impl;
+
+import com.smartquit.smartquitiot.service.AgoraService;
+import com.smartquit.smartquitiot.service.CoachService;
+import com.smartquit.smartquitiot.service.FeedbackService;
+
+public class AppointmentServiceImplTest {
+}
+
+
